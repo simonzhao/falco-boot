a8454881f6c872d07c330d0ebc4733e6167d5abc
zhaoyn <zhaoyn@mail.jj.cn>
2017-12-16 15:18:22 +0800

    增加对日志的引用

--- falco-boot-project/falco-boot/src/main/java/com/matrix001/falcoframework/boot/FalcoApplication.java
+++ falco-boot-project/falco-boot/src/main/java/com/matrix001/falcoframework/boot/FalcoApplication.java
@@ -1,20 +1,24 @@
 package com.matrix001.falcoframework.boot;
 
 import com.matrix001.falcoframework.context.ConfigurableApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FalcoApplication {
     private Class<?> primarySource;
+    private Logger logger = LoggerFactory.getLogger(FalcoApplication.class);
 
-    public FalcoApplication(Class<?> primarySource){
+    public FalcoApplication(Class<?> primarySource) {
         this.primarySource = primarySource;
     }
 
-    public ConfigurableApplicationContext run(String... args){
+    public ConfigurableApplicationContext run(String... args) {
         ConfigurableApplicationContext context = null;
+        logger.info("Hello World!");
         return context;
     }
 
-    public static ConfigurableApplicationContext run(Class<?> primarySource, String... args){
+    public static ConfigurableApplicationContext run(Class<?> primarySource, String... args) {
         return new FalcoApplication(primarySource).run(args);
     }
 }
