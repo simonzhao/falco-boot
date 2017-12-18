7afa371dd0870feb143ba207acc464706d81ca21
zhaoyn <zhaoyn@mail.jj.cn>
2017-12-18 20:44:28 +0800

    完成类的检索

--- falco-boot-project/falco-boot/src/main/java/com/matrix001/falcoframework/boot/FalcoApplication.java
+++ falco-boot-project/falco-boot/src/main/java/com/matrix001/falcoframework/boot/FalcoApplication.java
@@ -1,6 +1,9 @@
 package com.matrix001.falcoframework.boot;
 
 import com.matrix001.falcoframework.context.ConfigurableApplicationContext;
+import com.matrix001.falcoframework.util.ClassUtils;
+import com.matrix001.falcoframework.web.bind.annotation.RequestMapping;
+import com.matrix001.falcoframework.web.bind.annotation.RestController;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
@@ -9,8 +12,13 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import org.reflections.Reflections;
+import org.reflections.scanners.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 public class FalcoApplication {
     private Class<?> primarySource;
@@ -23,6 +31,34 @@ public class FalcoApplication {
     public ConfigurableApplicationContext run(String... args) {
         ConfigurableApplicationContext context = null;
         logger.info("Hello World!");
+
+        Package packages = this.primarySource.getPackage();
+        logger.debug("Package:[{}]", packages.getName());
+
+        Reflections reflections = new Reflections(packages.getName()
+                , new MethodAnnotationsScanner()
+                , new TypeAnnotationsScanner()
+                , new SubTypesScanner()
+                , new MethodParameterScanner()
+                , new MethodParameterNamesScanner()
+        );
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(RestController.class);
+
+        for(Class<?> clazz : classes){
+            logger.info("Class:[{}]", clazz.getName());
+        }
+        logger.info("Class num:[{}]", classes.size());
+
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(RequestMapping.class);
+        for(Method method : methods){
+            Class<?> clazz = method.getDeclaringClass();
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String[] paths = requestMapping.value();
+            for(String path : paths) {
+                logger.debug("Class:[{}] by [{}.{}]", path, clazz.getName(), method.getName());
+            }
+            logger.debug("Class:[{}.{}]", clazz.getName(), method.getName());
+        }
 
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
